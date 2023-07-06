1b18f93abae67c996ce667584ee9b33df52042e6
Osigelialex <osigelialex@gmail.com>
2023-07-06 12:37:48 +0100

    Fix

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,13 +3,13 @@
 Contains function to sum a list
 
 function:
- * sum_list: sums up a list
+ * sum_list: sums up a list    
 """
 
-from typing import Union, List
+from typing import List
 
 
-def sum_list(input_list: Union[List[int], List[float]]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Sums up a list of floats"""
     total_sum: float = 0.0
     for i in input_list:
