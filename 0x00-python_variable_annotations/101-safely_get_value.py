ec58ec12c8625ac863f73a73f9f1a40870dd1bf1
Osigelialex <osigelialex@gmail.com>
2023-07-06 11:12:32 +0100

    Advanced tasks

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+More involved type annotations
+"""
+
+from typing import TypeVar, Union, Mapping, Any
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:
+    """more involved type annotation"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
