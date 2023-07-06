2d9cca84fc4bc860988c8ff47a8593fe0121a191
Osigelialex <osigelialex@gmail.com>
2023-07-06 13:12:49 +0100

    Fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,6 +5,7 @@ Complex types - string and int/float to tuple
 
 from typing import List, Union, Tuple
 
-def to_kv(k: str, v: List[Union[int, float]]) -> Tuple[str, float]:
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple containing arguments"""
     return (k, v ** 2)
