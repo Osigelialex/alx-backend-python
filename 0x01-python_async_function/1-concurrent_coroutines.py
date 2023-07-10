a1461e6b623d0e22387908ce91d5a234ea680a42
Osigelialex <osigelialex@gmail.com>
2023-07-10 19:15:38 +0100

    Removed list comprehension

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,6 @@ functions:
 imports:
   * wait_random
 """
-import asyncio
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -16,11 +15,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawns wait_random n times with specified delay"""
-    # create a task for each coroutine which appends to a
-    # list of delays
     delays = []
 
-    for i in range(n):
-        delays.append(await wait_random(max_delay))
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
 
     return delays
