e185c1c3f56c07b4cd04b137cfcf3a425f289a1f
Osigelialex <osigelialex@gmail.com>
2023-07-11 19:09:22 +0100

    Changed type hint

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Async generator"""
 
-from typing import AsyncGenerator
+from typing import Generator
 import random
 import asyncio
 
 
-async def async_generator() -> AsyncGenerator:
+async def async_generator() -> Generator[float, None, None]:
     """Aynchronously yields random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
